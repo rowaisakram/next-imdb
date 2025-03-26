7ba50e98f54a38ffcc55bdabcb4e3972442ccdd5
rowaisakram <r.owaisakram@gmail.com>
2025-03-26 20:54:22 +0500

    Added the mongodb integration

--- src/app/webhooks/route.js
+++ src/app/webhooks/route.js
@@ -1,5 +1,6 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
+import { createOrUpdateUser, deleteUser } from "@/lib/actions/user";
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
@@ -53,14 +54,44 @@ export async function POST(req) {
   console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
   console.log("Webhook payload:", body);
 
-  if (eventType === "user.created") {
-    console.log("user created event");
-  }
-  if (eventType === "user.updated") {
-    console.log("user updated event");
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { first_name, last_name, image_url, email_addresses } = evt?.data;
+    try {
+      const user = await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses
+      );
+      if (user && eventType === "user.created") {
+        try {
+          const client = await clerkClient();
+          await client.users.updateUserMetadata(id, {
+            publicMetadata: {
+              userMongoId: user._id,
+            },
+          });
+        } catch (error) {
+          console.log("Error: Could not update user metadata: ", error);
+        }
+      }
+    } catch (error) {
+      console.log("Error: could not create or update user: ", error);
+      return new Response("Error: Could not create or update user", {
+        status: 400,
+      });
+    }
   }
   if (eventType === "user.deleted") {
-    console.log("user deleted event");
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.log("Error: Could not delete user: ", error);
+      return new Response("Error: Could not delete user", {
+        status: 400,
+      });
+    }
   }
 
   return new Response("Webhook received", { status: 200 });
