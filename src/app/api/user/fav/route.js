7205089455f10341637e5cacdeb8a120cd0631df
rowaisakram <r.owaisakram@gmail.com>
2025-05-09 17:13:15 +0500

    Added the logic for the fav

---
+++ src/app/api/user/fav/route.js
@@ -0,0 +1,59 @@
+import User from "@/lib/models/user.model";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import { connect } from "mongoose";
+
+export const PUT = async (req) => {
+  const user = await currentUser();
+  const client = await clerkClient();
+  try {
+    await connect();
+    const data = await req.json();
+    if (!user) {
+      return { status: 401, body: "Unauthorized" };
+    }
+    const existingUser = await User.findById(user.publicMetadata.userMongoId);
+    if (existingUser.favs.some((fav) => fav.movieId == data.movieId)) {
+      const updatedUser = await User.findByIdAndUpdate(
+        user.publicMetadata.userMongoId,
+        {
+          $pull: { favs: { movieId: data.movieId } },
+        },
+        { new: true }
+      );
+      const updatedFavs = updatedUser.favs.map((fav) => fav.movieId);
+      await client.users.updateUserMetadata(user.id, {
+        publicMetadata: {
+          favs: updatedFavs,
+        },
+      });
+      return new Response(JSON.stringify(updatedUser), { status: 200 });
+    } else {
+      const updatedUser = await User.findByIdAndUpdate(
+        user.publicMetadata.userMongoId,
+        {
+          $addToSet: {
+            fav: {
+              movieId: data.movieId,
+              title: data.title,
+              description: data.overview,
+              dateRelased: data.realseDate,
+              rating: data.voteCount,
+              image: data.image,
+            },
+          },
+        },
+        { new: true }
+      );
+      const updatedFavs = updatedUser.favs.map((fav) => fav.movieId);
+      await client.users.updateUserMetadata(user.id, {
+        publicMetadata: {
+          favs: updatedFavs,
+        },
+      });
+      return new Response(JSON.stringify(updatedUser), { status: 200 });
+    }
+  } catch (error) {
+    console.error("Error updating user:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+};
--- src/components/AddToFav.jsx
+++ src/components/AddToFav.jsx
@@ -1,5 +1,78 @@
-import React from "react";
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/compat/router";
+import React, { useEffect, useState } from "react";
 
-export default function AddToFav() {
-  return <div>AddToFav</div>;
+export default function AddToFav({
+  movieId,
+  title,
+  image,
+  overview,
+  realaseDate,
+  voteCount,
+}) {
+  const [isFav, setIsFav] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { isSignedIn, user, isLoaded } = useUser();
+  const router = useRouter();
+  useEffect(() => {
+    setIsLoading(true);
+    if (isLoaded && isSignedIn && user) {
+      setIsFav(user.publicMetadata?.favs?.includes(movieId));
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+    }
+  }, [movieId, isLoaded, isSignedIn, user]);
+
+  const handleFavClick = async () => {
+    setIsLoading(true);
+    if (!isSignedIn) {
+      setIsLoading(false);
+      router.push("/sgin-in");
+      return;
+    }
+    try {
+      const res = await fetch("/api/user/fav", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          movieId,
+          title,
+          overview,
+          realaseDate,
+          voteCount,
+          image,
+        }),
+      });
+      if (res.ok) {
+        setIsFav(!isFav);
+      } else {
+        console.log("Failed to update favorites");
+      }
+    } catch (error) {
+      console.error("Error ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div>
+      <button
+        onClick={handleFavClick}
+        className={`p-2 rounded ${
+          isFav ? "bg-red-300 dark:bg-red-600" : "bg-gray-300 dark:bg-gray-600"
+        }`}
+        disabled={isLoading}
+      >
+        {isLoading
+          ? "Loading..."
+          : isFav
+          ? "Remove from Favorites"
+          : "Add to Favorites"}
+      </button>
+    </div>
+  );
 }
